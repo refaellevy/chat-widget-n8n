7ad79848c58539c148f17e5a8a8881d721c00916
refaellevy <132892718+refaellevy@users.noreply.github.com>
2025-06-18 17:20:23 +0200

    Update chat-widget-n8n.js

--- chat-widget-n8n.js
+++ chat-widget-n8n.js
@@ -633,7 +633,7 @@
         <div class="chat-body">
             <div class="chat-messages"></div>
             <div class="chat-controls">
-                <textarea class="chat-textarea" placeholder="Type your message here..." rows="1"></textarea>
+                <textarea class="chat-textarea" placeholder="Tapez vos questions ici..." rows="1"></textarea>
                 <button class="chat-submit">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M22 2L11 13"></path>
